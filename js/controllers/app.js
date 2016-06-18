ec186fca77e69e752f431a52cc126a40d173683c
Jasonxu824 <fujisarki@gmail.com>
2016-06-18 12:51:01 +0800

    2016-6-18_1

--- js/controllers/app.js
+++ js/controllers/app.js
@@ -18,8 +18,36 @@ materialAdmin
                 },
                 delete: function() {
                     $scope.product.skus.splice($scope.product.skus.indexOf(this), 1);
+                },
+                deleteAttr: function(index) {
+                    this.values.splice(index, 1);
                 }
             });
             $scope.skuNameInput = '';
         }
+
+        $scope.nextStep = function(stepIndex) {
+            if (stepIndex < $scope.maxStep) {
+                $scope.stepIndex++;
+            }
+        }
+
+        $scope.preStep = function(stepIndex) {
+            if (stepIndex > 1) {
+                $scope.stepIndex--;
+            }
+        }
+
+        $scope.skuCombin = [];
+
+        $scope.$watch("stepIndex", function(value) {
+            if (value === 2) {
+                for (var i in $scope.product.skus) {
+                    var sku = $scope.product.skus[i]
+                    for (var j in sku .values) {
+                        console.log(sku.values[j]);
+                    }
+                }
+            }
+        });
     });
